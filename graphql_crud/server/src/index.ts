ee966767a4aeb77cc3dc3704e397e9aafa56c36d
Gaurav Mahawar <thegauravmahawar@gmail.com>
2023-06-08 19:12:48 +0530

    GraphQL CRUD - Setup Database and GraphQL interface

--- graphql_crud/server/src/index.ts
+++ graphql_crud/server/src/index.ts
@@ -1,19 +1,32 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
+import { schema } from './schema';
 import cors from 'cors'
 import { DataSource } from "typeorm";
 
 
 const main = async () => {
 
+    const dataSource = new DataSource({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'pguser',
+        password: 'pgpass',
+        database: 'pgdb',
+        logging: true,
+        synchronize: false,
+        entities: []
+    });
+
     const app = express()
     app.use(cors())
     app.use(express.json())
 
-    // app.use("/graphql", graphqlHTTP({
-    //     schema,
-    //     graphiql: true
-    // }))
+    app.use("/graphql", graphqlHTTP({
+        schema: schema,
+        graphiql: true
+    }))
 
     app.listen(3002, () => {
         console.log("Server running on port 3002.");
